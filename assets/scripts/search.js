b4383cdb7234aa0b6d514633fa9d0229bc35fd16
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-10-31 11:17:30 +0200

    change the  content of the card

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -18,7 +18,7 @@ function getMealList() {
               </div>
               <div class="meal-name">
                 <h3>${meal.strMeal}</h3>
-                <a href="#" class="recipe-btn">Get Recipe</a>
+                <a href="#" class="recipe-btn">ADD TO CART</a>
               </div>
             </div>
           `;
